baf41343b3c72f8c18bcd4f698dac77b3af8d20d
Christo Buschek <crito@30loops.net>
2011-11-01 02:41:23 +0100

    Fixed the python classifier.

--- setup.py
+++ setup.py
@@ -42,7 +42,7 @@ setup(
         'Operating System :: OS Independent',
         'Programming Language :: Python',
         'Programming Language :: Python :: 3',
-        'Topic :: Internet :: WWW/HTTP',
+        'Topic :: Software Development :: Testing',
     ],
     **extra
 )
