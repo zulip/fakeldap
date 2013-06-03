bc957141a3bfaf258a0859d65259fb883ec607b6
Marijn Vriens <marijnvriens@gmail.com>
2013-06-03 23:01:35 +0200

    Fixed bug where code was using ldap.NO_SUCH_OBJECT . ldap module was not imported. Switched to self.NO_SUCH_OBJECT .

--- fakeldap.py
+++ fakeldap.py
@@ -302,7 +302,7 @@ class MockLDAP(object):
         try:
             entry = self.directory[dn]
         except KeyError:
-            raise ldap.NO_SUCH_OBJECT
+            raise self.NO_SUCH_OBJECT
 
         for item in mod_attrs:
             op, key, value = item
@@ -349,7 +349,7 @@ class MockLDAP(object):
         try:
             del self.directory[dn]
         except KeyError:
-            raise ldap.NO_SUCH_OBJECT
+            raise self.NO_SUCH_OBJECT
 
         return (107, [])
 
@@ -370,7 +370,7 @@ class MockLDAP(object):
         attrs = self.directory.get(base)
         print attrs
         if attrs is None:
-            raise ldap.NO_SUCH_OBJECT
+            raise self.NO_SUCH_OBJECT
 
         return [(base, attrs)]
 
