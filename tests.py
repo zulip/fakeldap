daaddfb52ea82bfdf7b6711a7bd51ed78ee20c36
Mike Gabriel <mike.gabriel@das-netzwerkteam.de>
2020-11-30 15:50:33 +0100

    tests.py: Add test_modify_s_operation() unit test.

--- tests.py
+++ tests.py
@@ -59,6 +59,79 @@ class TestLdapOperations(unittest.TestCase):
                     "uid=bas,ou=people,dc=30loops,dc=net", record
                     ))
 
+    def test_modify_s_operation(self):
+        """Test the modification of records in the mock ldap object."""
+        directory = {
+            "ou=groups,dc=30loops,dc=net": { "ou": ("groups",) },
+            "cn=users,ou=groups,dc=30loops,dc=net": {
+                    "cn": ("users",),
+                    "memberUid": ( "john", "jack", "john2", "sam", "jim", "ben", ),
+                    },
+        }
+        self.mock_ldap = MockLDAP(directory)
+
+        modlist_MOD_ADD_non_present_attrdesc = [ (self.mock_ldap.MOD_ADD, 'description', 'Group of all users', ), ]
+        eq_((103,[]), self.mock_ldap.modify_s("cn=users,ou=groups,dc=30loops,dc=net", modlist_MOD_ADD_non_present_attrdesc))
+
+        self.assertEqual(self.mock_ldap.directory["cn=users,ou=groups,dc=30loops,dc=net"],
+            {
+                "cn": ("users",),
+                "description": ('Group of all users',),
+                "memberUid": ("john", "jack", "john2", "sam", "jim", "ben", ),
+            }
+        )
+
+        modlist_MOD_ADD_already_present_attrdesc = [ (self.mock_ldap.MOD_ADD, 'description', 'but not all users on the entire internet', ), ]
+        eq_((103,[]), self.mock_ldap.modify_s("cn=users,ou=groups,dc=30loops,dc=net", modlist_MOD_ADD_already_present_attrdesc))
+
+        self.assertEqual(self.mock_ldap.directory["cn=users,ou=groups,dc=30loops,dc=net"],
+            {
+                "cn": ("users",),
+                "description": ('Group of all users', 'but not all users on the entire internet',),
+                "memberUid": ("john", "jack", "john2", "sam", "jim", "ben", ),
+            }
+        )
+
+        modlist_MOD_DELETE_entire_attrdesc = [ (self.mock_ldap.MOD_DELETE, 'description', None, ), ]
+        eq_((103,[]), self.mock_ldap.modify_s("cn=users,ou=groups,dc=30loops,dc=net", modlist_MOD_DELETE_entire_attrdesc))
+
+        self.assertEqual(self.mock_ldap.directory["cn=users,ou=groups,dc=30loops,dc=net"],
+            {
+                "cn": ("users",),
+                "memberUid": ("john", "jack", "john2", "sam", "jim", "ben", ),
+            }
+        )
+
+        modlist_MOD_DELETE_one_value = [ (self.mock_ldap.MOD_DELETE, 'memberUid', 'jack', ), ]
+        eq_((103,[]), self.mock_ldap.modify_s("cn=users,ou=groups,dc=30loops,dc=net", modlist_MOD_DELETE_one_value))
+
+        self.assertEqual(self.mock_ldap.directory["cn=users,ou=groups,dc=30loops,dc=net"],
+            {
+                "cn": ("users",),
+                "memberUid": ("john", "john2", "sam", "jim", "ben", ),
+            }
+        )
+
+        modlist_MOD_DELETE_several_values = [ (self.mock_ldap.MOD_DELETE, 'memberUid', ['john', 'sam', 'ben'], ), ]
+        eq_((103,[]), self.mock_ldap.modify_s("cn=users,ou=groups,dc=30loops,dc=net", modlist_MOD_DELETE_several_values))
+
+        self.assertEqual(self.mock_ldap.directory["cn=users,ou=groups,dc=30loops,dc=net"],
+            {
+                "cn": ("users",),
+                "memberUid": ("john2", "jim", ),
+            }
+        )
+
+        modlist_MOD_REPLACE_all_different = [ (self.mock_ldap.MOD_REPLACE, 'memberUid', ['wilhelm', 'bernd', 'karl'], ), ]
+        eq_((103,[]), self.mock_ldap.modify_s("cn=users,ou=groups,dc=30loops,dc=net", modlist_MOD_REPLACE_all_different))
+
+        self.assertEqual(self.mock_ldap.directory["cn=users,ou=groups,dc=30loops,dc=net"],
+            {
+                "cn": ("users",),
+                "memberUid": ("wilhelm", "bernd", "karl", ),
+            }
+        )
+
     def test_search_s_base(self):
         result = self.mock_ldap.search_s("cn=admin,dc=30loops,dc=net", ldap.SCOPE_BASE)
         self.assertEqual(result, [('cn=admin,dc=30loops,dc=net', {'userPassword': 'ldaptest'})])
